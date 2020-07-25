ba9f2ad06b1bba816c0793ff6d274ef71f3d610a
Kyle Muldoon <kylemuldoon15@gmail.com>
2020-07-24 23:56:43 -0700

    added server.js, entry point for the backend api

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express(); // create express app
+
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("This is from express.js");
+});
+
+// start express server on port 5000
+app.listen(4000, () => {
+  console.log("server started on port 4000");
+});
