60f34bfafdfd586adbfaeb6f8e96b7d6fff5d8b3
Kyle Muldoon <kylemuldoon15@gmail.com>
2020-07-25 00:23:32 -0700

    Adding server start scripts

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,11 @@ const express = require("express");
 const app = express(); // create express app
 
 
-
-
-
-
-
-
 app.get("/", (req, res) => {
   res.send("This is from express.js");
 });
 
 // start express server on port 5000
-app.listen(4000, () => {
-  console.log("server started on port 4000");
+app.listen(8080, () => {
+  console.log("server started on port 8080");
 });
