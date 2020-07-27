68196f1765f7f895d96e7b85cc891f5c5b747907
Kyle Muldoon <kylemuldoon15@gmail.com>
2020-07-27 15:36:06 -0700

    commit

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,24 +1,43 @@
-import React from 'react'
-import "./Footer.css"
-import {fetchText} from './httpUtils.js'
+// import React, {useState, useEffect, useRef} from 'react'
+// import "./Footer.css"
+// /*
+// TODO:
+//     https://stackoverflow.com/questions/47658765/objects-are-not-valid-as-a-react-child-found-object-promise
+//     figure out how to pass reference of id="creator" into the fetch text function
+//         in order to update it once the asynchronous load completes.
+// */
+// export default function Footer() {
+//     return (
+//         <div id="Footer">
+//             <span id="creator">{}</span>
+//         </div>
+//     )
+// }
 
-let res = fetchText("/testing")
+import React, {Component} from 'react'
+import ReactDom from 'react-dom'
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {data: ""}
+    }
+
+    componentDidMount() {
+        fetch("/testing")
+            .then(res => res.text)
+            .then(text => this.setState({data: text}))
+    }
 
 
-/*
-TODO:
-    figure out how to pass reference of id="creator" into the fetch text function
-        in order to update it once the asynchronous load completes.
-*/
-
-
-export default function Footer() {
-
-
-
-    return (
-        <div id="Footer">
-            <span id="creator">{}</span>
-        </div>
-    )
+    render() {
+        return (
+            <div id="Footer">
+                <span id="creator">{toString(this.state.data)}</span>
+            </div>
+        )
+    }
 }
+
+export default App
--- frontend/src/httpUtils.js
+++
@@ -1,6 +0,0 @@
-export async function fetchText(url) {
-    let response = await fetch(url);
-    let data = await response.text();
-    console.log(data);
-    return data;
-}
